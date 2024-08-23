827de0a64f1e91c46bf209e99d8a253e996a7836
Stvim <steven.morin.fr@gmail.com>
2024-08-23 12:01:58 +0200

    test: for MatchesBundle / CompetitionsLibrary - calling the real backendApi to check the result

---
+++ sports-bets-iosTests/ViewModel/CompetitionsLibraryTests.swift
@@ -0,0 +1,28 @@
+//
+//  CompetitionsLibraryTests.swift
+//  sports-bets-iosTests
+//
+//  Created by Steven Morin on 22/08/2024.
+//
+
+import XCTest
+@testable import sports_bets_ios
+
+final class CompetitionsLibraryTests: XCTestCase {
+    var sut : CompetitionsLibrary!
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        sut = CompetitionsLibrary()
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+
+    func testPerformanceExample() throws {
+        
+    }
+
+}
---
+++ sports-bets-iosTests/ViewModel/MatchesBundleTests.swift
@@ -0,0 +1,46 @@
+//
+//  MatchesBundleTests.swift
+//  sports-bets-iosTests
+//
+//  Created by Steven Morin on 22/08/2024.
+//
+
+import XCTest
+@testable import sports_bets_ios
+
+final class MatchesBundleTests: XCTestCase {
+    typealias RemoteSut = MatchesBundle<RemoteMatchModel>
+    var sutRemote : RemoteSut!
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        sutRemote = RemoteSut()
+        
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sutRemote = nil
+    }
+
+    func test_sutRemote_fetchMatches_withCompetitionId2() async throws {
+        await sutRemote.fetchMatches(of: 2)
+        XCTAssertFalse(sutRemote.matches.isEmpty)
+    }
+    
+    func test_sutRemote_initMatches_withRemoteDataCompetitionId2_ShouldBeNotEmpty() async throws {
+        // FIXME: RemoteMatchModel repetition - should be RemoteSut.Match
+        let jsonData = try await WebApi.fetchData(for: RemoteMatchModel.RemoteApi.getAll(ofCompetitionId: 2))
+        let initializedMatches = try sutRemote.initMatches(from: jsonData)
+        
+        XCTAssertFalse(initializedMatches.isEmpty)
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
