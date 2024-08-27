3cb30cb462b45955b9588bb6474bb6223f88a1ab
Stvim <steven.morin.fr@gmail.com>
2024-08-27 18:09:37 +0200

    feat: MainView : Adding a user button on the top toolbar, just to show the login view as popover

--- sports-bets-ios/ContentView.swift
+++ sports-bets-ios/ContentView.swift
@@ -11,47 +11,29 @@ import SwiftData
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [Item]
+    @State private var loginSheetPresented = false
 
     var body: some View {
         NavigationSplitView {
             CompetitionsMenuView()
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: login) {
+                        Label("Login", systemImage: "person")
                     }
                 }
-                .onDelete(perform: deleteItems)
             }
-//            .toolbar {
-//                ToolbarItem(placement: .navigationBarTrailing) {
-//                    EditButton()
-//                }
-//                ToolbarItem {
-//                    Button(action: addItem) {
-//                        Label("Add Item", systemImage: "plus")
-//                    }
-//                }
-//            }
         } detail: {
             Text("Select an item")
         }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
+        .popover(isPresented: $loginSheetPresented) {
+            UserLoginView(userLogin: UserLogin())
         }
     }
 
-    private func deleteItems(offsets: IndexSet) {
+    private func login() {
         withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
+            loginSheetPresented = true
         }
     }
 }
