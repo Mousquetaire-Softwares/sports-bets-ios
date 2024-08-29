408dabc7d299a7f982e8d9724e6cc6be58151b0d
Stvim <steven.morin.fr@gmail.com>
2024-08-29 22:34:00 +0200

    refactor: moving extension of String (localized computed property) to a right place
    (cherry picked from commit ce9ac6a8009ff387e8a4f912ac21b79cdef6435a)

--- sports-bets-ios/View/UserLoginView.swift
+++ sports-bets-ios/View/UserLoginView.swift
@@ -107,11 +107,3 @@ struct UserLoginView: View {
     return UserLoginView(userLogin: userLogin)
 }
 
-extension String {
-    var localized: String {
-        return NSLocalizedString(self, comment: "")
-    }
-    func localized(arguments: CVarArg...) -> String {
-        return String(format: self.localized, arguments: arguments)
-    }
-}
