ce9ac6a8009ff387e8a4f912ac21b79cdef6435a
Stvim <steven.morin.fr@gmail.com>
2024-08-29 22:34:00 +0200

    refactor: moving extension of String (localized computed property) to a right place

--- sports-bets-ios/Common/Extensions/StringExtensions.swift
+++ sports-bets-ios/Common/Extensions/StringExtensions.swift
@@ -10,3 +10,13 @@ import Foundation
 extension String {
     static var empty = String("")
 }
+
+
+extension String {
+    var localized: String {
+        return NSLocalizedString(self, comment: "")
+    }
+    func localized(arguments: CVarArg...) -> String {
+        return String(format: self.localized, arguments: arguments)
+    }
+}
--- sports-bets-ios/View/UserLoginView.swift
+++ sports-bets-ios/View/UserLoginView.swift
@@ -104,11 +104,3 @@ struct UserLoginView: View {
     UserLoginView(userLogin: UserLogin())
 }
 
-extension String {
-    var localized: String {
-        return NSLocalizedString(self, comment: "")
-    }
-    func localized(arguments: CVarArg...) -> String {
-        return String(format: self.localized, arguments: arguments)
-    }
-}
