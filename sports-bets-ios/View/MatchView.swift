d999d1c2f195b861e032b6f3152a7904ffe54ccb
Stvim <steven.morin.fr@gmail.com>
2024-09-02 15:11:05 +0200

    feat: MatchView - Adding a preview with a fictive data binding

--- sports-bets-ios/View/MatchView.swift
+++ sports-bets-ios/View/MatchView.swift
@@ -101,7 +101,20 @@ struct Goals : ViewModifier {
 }
 
 
-//
-//#Preview {
-//    MatchView()
-//}
+#Preview {
+    struct PreviewContainer : View {
+        @State private var fictiveMatch = {
+            let fictiveData : Data = """
+{"Idt":265,"Num":3,"Hre":"21:00:00","Stade_Nom":"Stade olympique Lluís-Companys","Stade_Capacite":27,"Ville_Nom":"Barcelona","Fnt_cod":"ESP","Dte":"2023-09-19","DteHre":"2023-09-19T19:00:00.000Z","MatchTeam_Idt_Dom":188,"team_idt_Dom":30,"Nom_Dom":"FC Barcelone","Score_Dom":5,"MatchTeam_Idt_Ext":189,"team_idt_Ext":32,"Nom_Ext":"Royal Antwerp FC","Score_Ext":0,"Grp_Cod":"H","Journee_Lib":"1ère journée"}
+""".data(using: .utf8)!
+            let fictiveDTO = try! JSONDecoder().decode(RemoteMatchModel.RemoteDTO.self, from: fictiveData)
+            return RemoteMatchModel(remoteData: fictiveDTO)
+        }()
+        
+         var body: some View {
+             MatchView<RemoteMatchModel>(match: $fictiveMatch)
+         }
+    }
+
+    return PreviewContainer()
+}
