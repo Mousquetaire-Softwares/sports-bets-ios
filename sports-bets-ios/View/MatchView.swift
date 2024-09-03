8b97993ff027953485ca19a5a94044bd7d9f61fc
Stvim <steven.morin.fr@gmail.com>
2024-09-03 12:18:47 +0200

    feat:MatchView - Preview now shows 6 different use cases

--- sports-bets-ios/View/MatchView.swift
+++ sports-bets-ios/View/MatchView.swift
@@ -175,13 +175,61 @@ extension Goals {
         @State private var matches = {
             let datas : [Data] = [
 """
-{"Idt":265,"Num":3,"Hre":"21:00:00","Stade_Nom":"Stade olympique Lluís-Companys","Stade_Capacite":27,"Ville_Nom":"Barcelona","Fnt_cod":"ESP","Dte":"2023-09-19","DteHre":"2023-09-19T19:00:00.000Z","MatchTeam_Idt_Dom":188,"team_idt_Dom":30,"Nom_Dom":"FC Barcelone","Score_Dom":5,"MatchTeam_Idt_Ext":189,"team_idt_Ext":32,"Nom_Ext":"Royal Antwerp FC","Score_Ext":0,"Grp_Cod":"H","Journee_Lib":"1ère journée"}
+{"Idt":265,"Num":4,"Hre":"21:00:00","Stade_Nom":"Stade olympique Lluís-Companys","Stade_Capacite":27,"Ville_Nom":"Barcelona","Fnt_cod":"ESP","Dte":"2023-09-19","DteHre":"2023-09-19T19:00:00.000Z","MatchTeam_Idt_Dom":188,"team_idt_Dom":30,"Nom_Dom":"FC Barcelone","Score_Dom":5,"MatchTeam_Idt_Ext":189,"team_idt_Ext":32,"Nom_Ext":"Royal Antwerp FC","Score_Ext":0,"Grp_Cod":"H","Journee_Lib":"1ère journée"}
 ""","""
 {"Idt":264,"Num":2,"Hre":"18:45:00","Stade_Nom":"Stade du Wankdorf","Stade_Capacite":23,"Ville_Nom":"Berne","Fnt_cod":"SUI","Dte":"2023-09-19","DteHre":"2023-09-19T16:45:00.000Z","MatchTeam_Idt_Dom":186,"team_idt_Dom":25,"Nom_Dom":"BSC Young Boys","MatchTeam_Idt_Ext":187,"team_idt_Ext":27,"Nom_Ext":"RB Leipzig","Grp_Cod":"G","Journee_Lib":"1ère journée"}
 """
             ].map{ $0.data(using: .utf8)! }
             let dtos = datas.map{ try! JSONDecoder().decode(RemoteMatchModel.RemoteDTO.self, from: $0) }
-            return dtos.map { RemoteMatchModel(remoteData: $0) }
+            let matchesTemplates = dtos.map { RemoteMatchModel(remoteData: $0) }
+            
+            var matchesPreview = [RemoteMatchModel]()
+            
+            matchesPreview.append( {
+                var match = matchesTemplates.first!
+                match.userHasRegistered = false
+                match.localTeamScoreBet = 9
+                match.externalTeamScoreBet = 9
+                return match
+            }())
+            matchesPreview.append( {
+                var match = matchesTemplates.first!
+                match.userHasRegistered = true
+                match.localTeamScoreBet = 2
+                match.externalTeamScoreBet = 99
+                return match
+            }())
+            matchesPreview.append( {
+                var match = matchesTemplates.first!
+                match.userHasRegistered = true
+                match.localTeamScoreBet = nil
+                match.externalTeamScoreBet = nil
+                return match
+            }())
+                        
+            matchesPreview.append( {
+                var match = matchesTemplates.last!
+                match.userHasRegistered = false
+                match.localTeamScoreBet = 9
+                match.externalTeamScoreBet = 9
+                return match
+            }())
+            matchesPreview.append( {
+                var match = matchesTemplates.last!
+                match.userHasRegistered = true
+                match.localTeamScoreBet = 2
+                match.externalTeamScoreBet = 99
+                return match
+            }())
+            matchesPreview.append( {
+                var match = matchesTemplates.last!
+                match.userHasRegistered = true
+                match.localTeamScoreBet = nil
+                match.externalTeamScoreBet = nil
+                return match
+            }())
+            
+            return matchesPreview
         }()
         
          var body: some View {
