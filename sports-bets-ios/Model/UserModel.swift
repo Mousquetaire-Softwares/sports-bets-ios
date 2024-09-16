a05a5cb9f22e9bba371d9e54d5f6f4bb5bb412fe
Stvim <steven.morin.fr@gmail.com>
2024-09-16 12:01:44 +0200

    feat: UserLogged is now autosaved and autoloaded in UserDefaults

--- sports-bets-ios/Model/UserModel.swift
+++ sports-bets-ios/Model/UserModel.swift
@@ -11,6 +11,8 @@ struct UserModel {
     typealias RemoteApi = BackendApi.User.Login
     typealias RemoteUserDTO = RemoteApi.DTO.User
     
+    typealias Contents = [String:String]
+    
     var token: String
     
     let username, email, title, role: String
@@ -24,5 +26,34 @@ struct UserModel {
         self.role = userDto.role
         self.firstName = userDto.firstName
         self.lastName = userDto.lastName
+    }
+    
+    init(fromContents dic:Contents) throws {
+        let getValue = {
+            key in
+            guard let value = dic[key] else {
+                throw CocoaError(.coderValueNotFound)
+            }
+            return value
+        }
+        self.token = try getValue("token")
+        self.username = try getValue("username")
+        self.email = try getValue("email")
+        self.title = try getValue("title")
+        self.role = try getValue("role")
+        self.firstName = try getValue("firstName")
+        self.lastName = try getValue("lastName")
+    }
+    
+    func contents() -> Contents {
+        var dic : Contents = [:]
+        dic["token"] = token
+        dic["username"] = username
+        dic["email"] = email
+        dic["title"] = title
+        dic["role"] = role
+        dic["firstName"] = firstName
+        dic["lastName"] = lastName
+        return dic
     }
 }
--- sports-bets-ios/ViewModel/UserLogged.swift
+++ sports-bets-ios/ViewModel/UserLogged.swift
@@ -8,11 +8,53 @@
 import Foundation
 
 class UserLogged : ObservableObject {
-    @Published private(set) var user : UserModel?
+    @Published private(set) var user : UserModel? {
+        didSet {
+            autosave()
+        }
+    }
     var token : String? { user?.token }
     var isSet : Bool { user != nil }
     
+    init() {
+        autoload()
+    }
+    
+    private func autosave() {
+        save(instance: dataAutosaveInstanceName)
+    }
+    private func autoload() {
+        load(instance: dataAutosaveInstanceName)
+    }
+    func save(instance:String) {
+        let key = dataKey(instance: instance)
+        let contents = user?.contents()
+        userDefaults.setValue(contents, forKey: key)
+    }
+    func load(instance:String) {
+        let key = dataKey(instance: instance)
+        if let loadedContents = userDefaults.userModelContents(forKey: key) {
+            do {
+                self.user = try UserModel(fromContents: loadedContents)
+            } catch {
+                print("\(Self.self): error while reading data from userDefaults \(key) : \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    private let userDefaults = UserDefaults.standard
+    private let dataBaseKey = "UserLogged"
+    private let dataAutosaveInstanceName = "autosave"
+    private func dataKey(instance:String) -> String { dataBaseKey + "_" + instance }
+    
     func setUser(_ user: UserModel?) {
         self.user = user
+    }
+}
+
+
+extension UserDefaults {
+    func userModelContents(forKey key:String) -> UserModel.Contents? {
+        return dictionary(forKey: key) as? UserModel.Contents
     }
 }
